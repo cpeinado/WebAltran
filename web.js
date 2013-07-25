01e6508319731c87aabbfd02d3e19bca6a54555a
cpeinado <cpeinado@gmail.com>
2013-07-25 21:38:17 +0200

    first commit

---
+++ web.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require ('fs');
+
+var Buff = new Buffer('');
+
+Buff = fs.readFileSync("html/index.html");
+
+app.use(express.static(__dirname));
+
+app.get('/', function(request, response) {
+  //response.send('Hello Isi y N3!');
+    response.send(Buff.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
